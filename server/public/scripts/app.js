e30d5f09e681b771ca3cbdb7cf72b5b02ea210d1
Dave Gamberg <dgamberg@gmail.com>
2015-10-25 20:14:44 -0500

    adding styles, redo divs, added slide timer

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -5,38 +5,39 @@ $(document).ready(function(){
    start();
 });
 
-
 function start(){
     $.ajax({
         type: "GET",
         url: "/data",
         success: function(data){
             zetaArray = data.zeta;
-            init(zetaArray);
+            createCarousel(zetaArray);
+            updateIndexPoints();
+            $("#next").on('click', nextSlide);
+            $("#prev").on('click', prevSlide);
+            startSlideTimer();
         }
     });
 }
-function init(data){
-    createCarousel(zetaArray);
-    updateIndexPoints();
-    $("#next").on('click', nextSlide);
-    $("#prev").on('click', prevSlide);
+function startSlideTimer(){
+    setInterval(function(){ nextSlide(); }, 3000);
 }
 
 function createCarousel(array){
-    $("#carousel").append("<div class='main'></div>");
-    var $el = $("#carousel").children().last();
+    $("#carouselControls").append("<div class='main'></div>");
+    var $el = $("#carouselControls").children().last();
     createNavButtons($el);
     createIndexPoints(array, $el);
 }
 
 function nextSlide(){
-    $('.person-container').css('opacity', '0');
+    $('.person-container').fadeOut();
     indexTracker++;
     if(indexTracker >= zetaArray.length){
         indexTracker = 0;
     }
     updateIndexPoints();
+    $('.person-container').fadeIn();
 }
 
 function prevSlide(){
@@ -48,8 +49,8 @@ function prevSlide(){
 }
 
 function createNavButtons($el){
-    $el.append("<div id='prev' class='nav-button'>Prev</div>");
-    $el.append("<div id='next' class='nav-button'>Next</div>");
+    $el.append("<div id='prev' class='nav-button'><<</div>");
+    $el.append("<div id='next' class='nav-button'>>></div>");
 }
 
 function createIndexPoints(array, $el){
@@ -61,13 +62,9 @@ function createIndexPoints(array, $el){
 }
 
 function updateIndexPoints(){
-
     for(var i = 0; i < zetaArray.length; i++){
-
         $("#index" + i).removeClass("index-point-active");
-
         if(i == indexTracker){
-
             $("#index" + i).addClass("index-point-active");
             displaySlide(i);
         }
@@ -75,12 +72,12 @@ function updateIndexPoints(){
 }
 
 function displaySlide(i){
-    var slideOutput =  "<div id='person" + [i] +"' class='person-container well'>" +
-        "<span class='zetaName'>" + zetaArray[i].name + "</span>" +
-        "<img class='git-image' src='" + zetaArray[i].imageUrl + "'>" +
-        "<blockquote>" + zetaArray[i].shoutout + "</blockquote>" +
-        "<div class='github-link'><a target='_blank' href='" + zetaArray[i].github + "'>GITHUB LINK >></a>" +
-        "</div></div></div>";
-
+    var slideOutput =
+        "<div id='person" + [i] +"' class='person-container well'>" +
+            "<div class='zetaName'>" + zetaArray[i].name + "</div>" +
+            "<div><img class='git-image' src='" + zetaArray[i].imageUrl + "'></div>" +
+            "<div class='shoutout'>" + zetaArray[i].shoutout + "</div>" +
+            "<div class='github-link'><a target='_blank' href='" + zetaArray[i].github + "'>GITHUB LINK >></a></div>" +
+        "</div>"; //end person
     $('#mainContent').html(slideOutput);
 }
